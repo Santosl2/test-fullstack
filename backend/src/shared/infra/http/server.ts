19a2dc6708d0024739e2cce01a36d79ac1fde429
Santosl2c <mfilype2019@gmail.com>
2022-03-05 16:10:39 -0300

    feat: typeorm and bcrypt implements

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,9 +1,13 @@
+/* eslint-disable import/extensions */
 import "reflect-metadata";
 
 import express from "express";
+// eslint-disable-next-line import/no-unresolved
 import "../typeorm";
 
 const app = express();
+
+app.use(express.json());
 
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
